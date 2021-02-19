b2b984066a43d40c04a85b9afc15fe88f694e112
baraahhawari <baraah.hawari@gmail.com>
2021-02-19 21:57:18 +0200

    Flexbox_and_Templating

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Photo_Gallery_JSON_files :smiley:
+# JSON_files_photo_gallery :smiley:
+
+Create a photo gallery using the provided JSON files and styled it using floats.
 
 ### Feature #1: Display images :alien:
 
@@ -20,3 +22,36 @@
 
 1. The user can sort the images by either title or by a number of horns.
 1. Sort the images by one of the properties on page load. Also, this will be applied to the second page of images.
+
+---
+
+### Feature 1: Pagination :grinning:
+
+1. A navigation added for the user to switch between two pages. Each page will render a unique set of images from one of the two provided JSON files.
+1. The filters will be populated using only keywords from the images currently being displayed.
+
+### Feature 2: Templating :smirk:
+
+1. The appropriate Mustache template created in the HTML with the same `<h2>`, `<img>`, and `<p>` elements as the jQuery template from the prior lab.
+1. The method that renders the images refactored to use Mustache instead of making a copy with jQuery.
+
+### Feature 3: Styling with Flexbox :smile:
+
+1. The CSS refacored to use `Flexbox` instead of floats.
+
+### Feature 4: Sort the images :green_heart:
+
+1. The user will have the ability to sort the images by either title or by number of horns.
+1. Sort the images by one of the properties on page load. This should also apply to the second page of images.
+
+### Stretch Goal: Detail view :scream_cat:
+
+1. Add a detail view which will display the image in a larger size in the center of the screen with a colored background.
+1. The description should be shown now, as well.
+   When the user clicks off of the image, return to the grid view.
+1. Use a transition or animation to show and hide the detail view of an image.
+
+### Stretch Goal: Fuzzy search :scream_cat:
+
+1. Create an input element to allow users to search for an image. It is up to you and your partner to decide if the user should be able to search by title, keyword, or both.
+1. Write a regular expression pattern to create a fuzzy search so that the results are narrowed down and displayed every time the user enters or removes a character from the input.
--- js/app.js
+++ js/app.js
@@ -4,81 +4,127 @@
 var hornsKeyWords = [];
 AnimalCard.all = [];
 
-// Constructor
-
+// Constructor for all animals
 function AnimalCard(data) {
   this.image_url = data.image_url;
   this.title = data.title;
   this.keyword = data.keyword;
   this.description = data.description;
   this.horns = data.horns;
-
-  hornsKeyWords.push(this.keyword);
+  // all
   AnimalCard.all.push(this);
+  // unique keywords
+  if (!hornsKeyWords.includes(this.keyword)) {
+    hornsKeyWords.push(this.keyword);
+  }
 }
 
 // render the data prototype function
 AnimalCard.prototype.render = function () {
-  // clone the template to avoid re-appending more than once (class of the div that contain the template) jQuery
-  let picsTemplate = $('.horn-template').clone();
-
-  // find and set value into the markup
-  picsTemplate.find('h3').text(this.title);
-  picsTemplate.find('img').attr('src', this.image_url);
-  picsTemplate.find('p').text(this.description);
-
-  // remove the class from the origin template to get the cloned one
-  picsTemplate.removeClass('horn-template');
-
-  // give each div a class to help to made the selection
-  picsTemplate.addClass(this.keyword);
-
+  // Mustache 
+  let picsTemplate = $('#theTemplate').html();
   //  append to the main container in the HTML
-  $('main').append(picsTemplate);
+  $('main').append(Mustache.render(picsTemplate, this));
 }
+
+
+// keyword selections
+function thekeyWords() {
+  hornsKeyWords.forEach(element => {
+    let theKey = new Thekey(element);
+    Thekey.all.push(theKey);
+  });
+  Thekey.all.forEach(element => {
+    let theOption = $('#option').html();
+    $('#selectMenu').append(Mustache.render(theOption, element));
+  })
+}
+
+// keywords constructor
+function Thekey(ele) {
+  this.keyOption = ele;
+}
+
+// array
+Thekey.all = [];
+
+// reading info from 2 JSONs pages function
+function thePages() {
+  $('button').on('click', function () {
+    let pageAttr = $(this).attr('id');
+    // console.log(pageAttr);
+    $('div').remove();
+    hornsKeyWords = [];
+    AnimalCard.all = [];
+    Thekey.all = [];
+    renderPage(`./data/${pageAttr}.json`);
+  })
+}
+thePages();
 
 // rendering the options in the select tag
 function selectOption() {
-  AnimalCard.all.forEach(hornPic => {
-    //check if the keyword is not douplicated to render it
-    if (!AnimalCard.all.includes(hornPic.keyword)) {
-      AnimalCard.all.push(hornPic.keyword);
-      //append the option to the select
-      $('#selectMenu').append(
-        `<option value='${hornPic.keyword}'> ${hornPic.keyword} </option>`
-      );
+  $('#selectMenu').on('change', function () {
+    let selected = $(this).val();
+    // console.log(selected);
+    $('div').hide();
+    if (selected === 'default') {
+      $('div').show();
     }
-  });
+    AnimalCard.all.forEach(element => {
+      if (element.keyword === selected) {
+        $(`.${selected}`).show();
+      }
+    })
+  })
 }
 
-// show the selected items only
-$('#selectMenu').on('change', function () {
-  //like the event listener
-  let selected = $(this).val();
-  // hide all the dives
-  $('.templateId').hide();
-  // show only the selected ones with fadein animation (.)=> for the class added before
-  $(`.${selected}`).fadeIn(900);
-});
+// Sorting function
+function sortFun() {
+  $('#sortBy').on('click', function () {
+    if ($('#sortBy').val() === 'title') {
+      console.log('title');
+      theAlgorithm(AnimalCard.all, 'title');
+    } else if ($('#sortBy').val() === 'horns') {
+      console.log('horns');
+      theAlgorithm(AnimalCard.all, 'horns');
+    }
+  })
+}
 
-// Reading Data Ajax
-function readData() {
-  const ajaxSettings = {
-    method: 'get',
-    dataType: 'json'
-  };
+// sorting Algorithm
+function theAlgorithm(arr, val) {
+  arr.sort((a, b) => {
+    let first = a[val];
+    let second = b[val];
 
-  $.ajax('data/page-1.json', ajaxSettings)
-    .then(data => {
-      // console.log(data);
-      data.forEach(element => {
-        let animal = new AnimalCard(element);
-        // console.log(animal, 'hello');
-        animal.render();
-      });
-      selectOption();
+    if (first < second) {
+      return -1;
+    } else if (first > second) {
+      return 1;
+    } else {
+      return 0;
+    }
+  })
+  $('div').remove();
+  AnimalCard.all.forEach(ele => {
+    ele.render();
+  })
+}
+
+//// Reading Data Ajax JSON files
+function renderPage(JSONData) {
+  $.get(JSONData).then(data => {
+    data.forEach(element => {
+      let newAnimal = new AnimalCard(element);
+      // console.log(newAnimal);
+      newAnimal.render();
     });
+    thekeyWords();
+    selectOption();
+    sortFun();
+  })
 }
 
-// calling funcitons
-$('document').ready(readData);
+// calling funciton
+$('document').ready(renderPage(`data/page-1.json`));
